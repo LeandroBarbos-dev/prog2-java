638dcb49dd09f8d8d9b2f4d23fcc437dafbd5027
Leandro <leandrobpa.dev@gmail.com>
2026-05-26 16:31:17 -0300

    v-26-05-26_2

--- prog2/src/lista5/ex1/RelatorioVendas.java
+++ prog2/src/lista5/ex1/RelatorioVendas.java
@@ -1,5 +1,14 @@
 package lista5.ex1;
 
-public class RelatorioVendas {
+public class RelatorioVendas extends GeradorRelatorio{
 
+	
+	
+	public void processarDados() {
+		
+	}
+	
+	public void gerar(Formatador formatador) {
+		
+	}
 }
